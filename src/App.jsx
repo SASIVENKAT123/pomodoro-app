9f03b05d8412243241b0896cdf9a3d338ed94e08
tej-2206 <teja.thota@gtplind.com>
2026-06-17 17:39:26 +0530

    onboarding part added and session storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 
 import { useState, useEffect, useRef} from "react";
+import OnboardingModal from "./components/OnboardingModal";
 
 function App() {
+  const getSessionData = () => {
+
+    const sessionData = sessionStorage.getItem("pomodoroSession");
+      return sessionData? JSON.parse(sessionData) : null;
+  };
 
   // step:1 State (app memory)
-  const [minutes, setMinutes] = useState(25)
+
+  const [userData, setUserData]  = useState(() => getSessionData());
+  const[showOnboarding, setShowOnboarding] = useState(() => {
+    const data = getSessionData();
+    return !data;
+  });
+  const [showPlanModal, setShowPlanModal] = useState(false);
+  const [planQueue, setPlanQueue] = useState([]);
+
+  const [minutes, setMinutes] = useState(() => {
+    const data = getSessionData();
+    return data ? data.focusTime :25;
+  });
   const [seconds, setSeconds] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
   const [isBreak, setIsBreak] = useState(false)
   const [totalSec, setTotalSec]= useState(0)
+
+
+
+
+  const handleGenricPomodoro = (data) => {
+    sessionStorage.setItem("pomodoroSession", JSON.stringify(data));
+    setUserData(data);
+    setMinutes(data.focusTime);
+    setShowOnboarding(false);
+  }
+
+  const handleInitialPlan = (data) => {
+    setUserData(data);
+    setShowOnboarding(false);
+    setShowPlanModal(true);
+  }
 
   const alarm = useRef(typeof Audio!== "undefined"? new Audio("/timer.mp3"):null );
   // step 2: the countdown engine
@@ -29,15 +63,19 @@ function App() {
 
         if (alarm.current){
           alarm.current.play().catch(err => console.error("Audio error:", err));
+
         }
+
+        const configuredFocus = userData?.focusTime || 25
+        const configuredBreak = userData?.breakTime || 5
         //timer hits 00 : 00 -- switch modes
         if (!isBreak) {
           setIsBreak(true)
-          setMinutes(5)
+          setMinutes(configuredBreak)
           setSeconds(0)
         } else {
           setIsBreak(false)
-          setMinutes(25)
+          setMinutes(configuredFocus)
           setSeconds(0)
 
         }
@@ -49,7 +87,7 @@ function App() {
     return () => clearInterval(interval) //cleanup when paused or unmounted
 
   
-  }, [isRunning, minutes, seconds, isBreak])
+  }, [isRunning, minutes, seconds, isBreak, userData])
 
   //step-3 : helper functions
   const pad = (n) => String(n).padStart(2, "0")
@@ -60,47 +98,59 @@ function App() {
 
   //step - 4 ui jsx
   return (
+    <>
 
-    <div className={`min-h-screen flex flex-col transition-colors duration-700 ${isBreak ? "bg-blue-950" : "bg-green-900"}`}>
+      {showOnboarding && (
+        <OnboardingModal
+          onComplete={handleInitialPlan}
+          onGeneric={handleGenricPomodoro}
+        />
+      )}
 
-    {/* //header */}
+      {!showOnboarding && !showPlanModal && (
 
-      <header className="flex justify-between items-center px-8 py-4">
-        <div className="flex items-center gap-3">
-          <div className="w-10 h-10 rounded-full bg-white/25" />
-            <span className ="text-[#D4AF37] font-bold font-sans">Welcome, <strong>ZORO</strong></span>
-          
+        <div className={`min-h-screen flex flex-col transition-colors duration-700 ${isBreak ? "bg-blue-950" : "bg-green-900"}`}>
+
+        {/* //header */}
+
+          <header className="flex justify-between items-center px-8 py-4">
+            <div className="flex items-center gap-3">
+              <div className="w-10 h-10 rounded-full bg-white/25" />
+                <span className ="text-[#D4AF37] font-bold font-sans">Welcome, <strong>{userData?.name|| "Guest"}</strong></span>
+              
+            </div>
+
+            <nav className ="flex gap-6 text-white/50 text-sm font-sans">
+              <a href="#" className="hover:text-white">Plan your day</a>
+              <a href="#" className="hover:text-white">Change Timer</a>
+              <a href="#" className="hover:text-white">Change color</a>
+            </nav>
+          </header>
+
+          <main className="flex-1 flex flex-col items-center justify-center">
+            <p className="text-white/100 text-sm uppercase tracking-widest mb-0 font-sans">
+              {isBreak ? "☕ Break Time" : "🎯 Focus Time"}
+            </p>
+
+            <div className="text-white text-center w-[70%] font-mono font-black" style={{fontSize: "clamp(4rem, 15vw, 20rem)"}}>
+              {pad(minutes)}:{pad(seconds)}
+            </div>
+
+            <button onClick={() => setIsRunning(!isRunning)} className="mt-2 mb-8 w-170 px-8 py-4 bg-black text-white rounded-full text-sm tracking-widest uppercase font-bold font-sans hover:bg-red-900 transition-colors">
+              {isRunning ? "PAUSE" : "START TIMER"}
+            </button>
+
+
+          </main>
+          <footer className="text-center py-5 text-white/50 text-lg font-sans">
+            You have been productive for {" "}
+            <strong className="text-white/80">"{pad(focusMin)} : {pad(focusSec)}"</strong>
+            minutes today - keep going <strong className="text-white/80">{userData?.name || "Guest"}</strong>
+            
+          </footer>
         </div>
-
-        <nav className ="flex gap-6 text-white/50 text-sm font-sans">
-          <a href="#" className="hover:text-red-900">Plan your day</a>
-          <a href="#" className="hover:text-white">Change Timer</a>
-          <a href="#" className="hover:text-white">Change color</a>
-        </nav>
-      </header>
-
-      <main className="flex-1 flex flex-col items-center justify-center">
-        <p className="text-white/100 text-sm uppercase tracking-widest mb-0 font-sans">
-          {isBreak ? "☕ Break Time" : "🎯 Focus Time"}
-        </p>
-
-        <div className="text-white text-center w-[70%] font-mono font-black" style={{fontSize: "clamp(4rem, 15vw, 20rem)"}}>
-          {pad(minutes)}:{pad(seconds)}
-        </div>
-
-        <button onClick={() => setIsRunning(!isRunning)} className="mt-2 mb-8 w-170 px-8 py-4 bg-black text-white rounded-full text-sm tracking-widest uppercase font-bold font-sans hover:bg-red-900 transition-colors">
-          {isRunning ? "PAUSE" : "START TIMER"}
-        </button>
-
-
-      </main>
-      <footer className="text-center py-5 text-white/50 text-lg font-sans">
-        You have been productive for {" "}
-        <strong className="text-white/80">"{pad(focusMin)} : {pad(focusSec)}"</strong>
-        minutes today - keep going <strong className="text-white/80">"ZORO"</strong>
-        
-      </footer>
-    </div>
+      )}
+    </>
   )
   
 }
